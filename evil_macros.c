e606d39deb04eaff5875967dc1c5a13868775261
Your Name <you@example.com>
2020-08-31 18:59:05 -0400

    Only commit

---
+++ evil_macros.c
@@ -0,0 +1,57 @@
+#define 👉 {
+#define 👈 }
+#define 👉👈 = 
+#define 😵 !
+#define 👈👉 +
+#define 👈👈 -
+
+#define 😡 / // angry, division
+
+#define 👇 *
+#define 📥 & // finding where something is
+
+#define 🏃 goto
+#define 🔄 for
+#define 🔁 while
+#define 🔀 switch
+#define 💼 case // briefcase
+#define 💤 default // the default option is boring
+
+#define 💥 break
+#define 🔂 continue
+
+#define 🤔 if
+#define 🤔🤔 else if
+#define 🤔🤔🤔 else
+
+#define 🤾 return
+
+#define 💀 void
+#define 🖖 double
+#define 🖕 int
+#define 👻 float
+#define 🩳 short
+#define 👖 long
+#define 🏥 char // a pun on care!
+
+#define 🟢 signed // a go sign
+#define 🛑 unsigned // a stop sign
+
+#define 📧🔢 enum
+#define 🏢 struct
+#define 🤝 union
+
+#define 🧊 const // frozen, won't change
+
+#define 🕰️ sizeof
+#define 🕵️ typedef // detectives have aliases - alias type
+#define 😷 volatile
+
+#define 🥶 static
+#define 🚓 auto // like a car
+#define 💵 register // a dollar from a cash register
+#define 🗺️ extern // other location, global
+
+🖕 main() 👉
+	👖 bigNum 👉👈 254322345;
+👈
